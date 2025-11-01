3a2e553d52274e71f1a41899c558e5aa1743a2e3
Mostafa <m0stafas889a@gmail.com>
2025-11-01 03:24:32 -0700

    "autosyncing agent activities"

--- verilog/dv/cocotb/system_integration_test/system_integration_test.c
+++ verilog/dv/cocotb/system_integration_test/system_integration_test.c
@@ -17,13 +17,14 @@ void main(void) {
     
     int pass = 1;
     
-    USER_writeWord(0x12345678, SRAM0_BASE + (0 << 2));
-    USER_writeWord(0x9ABCDEF0, SRAM0_BASE + (10 << 2));
-    USER_writeWord(0xDEADBEEF, SRAM0_BASE + (100 << 2));
+    uint32_t sram0_base_offset = (SRAM0_BASE - 0x30000000) >> 2;
+    USER_writeWord(0x12345678, sram0_base_offset + 0);
+    USER_writeWord(0x9ABCDEF0, sram0_base_offset + 10);
+    USER_writeWord(0xDEADBEEF, sram0_base_offset + 100);
     
-    uint32_t val1 = USER_readWord(SRAM0_BASE + (0 << 2));
-    uint32_t val2 = USER_readWord(SRAM0_BASE + (10 << 2));
-    uint32_t val3 = USER_readWord(SRAM0_BASE + (100 << 2));
+    uint32_t val1 = USER_readWord(sram0_base_offset + 0);
+    uint32_t val2 = USER_readWord(sram0_base_offset + 10);
+    uint32_t val3 = USER_readWord(sram0_base_offset + 100);
     
     if (val1 != 0x12345678 || val2 != 0x9ABCDEF0 || val3 != 0xDEADBEEF) {
         pass = 0;
@@ -31,13 +32,14 @@ void main(void) {
     
     send_pulse(1);
     
-    USER_writeWord(0xCAFEBABE, SRAM1_BASE + (0 << 2));
-    USER_writeWord(0xFEEDFACE, SRAM1_BASE + (20 << 2));
-    USER_writeWord(0xC0FFEE00, SRAM1_BASE + (200 << 2));
+    uint32_t sram1_base_offset = (SRAM1_BASE - 0x30000000) >> 2;
+    USER_writeWord(0xCAFEBABE, sram1_base_offset + 0);
+    USER_writeWord(0xFEEDFACE, sram1_base_offset + 20);
+    USER_writeWord(0xC0FFEE00, sram1_base_offset + 200);
     
-    val1 = USER_readWord(SRAM1_BASE + (0 << 2));
-    val2 = USER_readWord(SRAM1_BASE + (20 << 2));
-    val3 = USER_readWord(SRAM1_BASE + (200 << 2));
+    val1 = USER_readWord(sram1_base_offset + 0);
+    val2 = USER_readWord(sram1_base_offset + 20);
+    val3 = USER_readWord(sram1_base_offset + 200);
     
     if (val1 != 0xCAFEBABE || val2 != 0xFEEDFACE || val3 != 0xC0FFEE00) {
         pass = 0;
@@ -45,8 +47,8 @@ void main(void) {
     
     send_pulse(1);
     
-    val1 = USER_readWord(SRAM0_BASE + (0 << 2));
-    val2 = USER_readWord(SRAM0_BASE + (10 << 2));
+    val1 = USER_readWord(sram0_base_offset + 0);
+    val2 = USER_readWord(sram0_base_offset + 10);
     if (val1 != 0x12345678 || val2 != 0x9ABCDEF0) {
         pass = 0;
     }
