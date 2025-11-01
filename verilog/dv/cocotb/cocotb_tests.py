adb15e66304869049f0668af66e6e92d0e975a81
Mostafa <m0stafas889a@gmail.com>
2025-11-01 07:00:36 -0700

    "autosyncing agent activities"

--- verilog/dv/cocotb/cocotb_tests.py
+++ verilog/dv/cocotb/cocotb_tests.py
@@ -1,11 +1,3 @@
-from caravel_cocotb.tests.common_functions.test_functions import *
-from caravel_cocotb.tests.housekeeping.housekeeping import *
-
-def register_tests(dut):
-    tests = []
-    
-    tests.append(('comprehensive_address_test', 'comprehensive_address_test/comprehensive_address_test.py', 'comprehensive_address_test/comprehensive_address_test.c', {'sim': ['RTL', 'GL', 'GL_SDF']}))
-    tests.append(('access_type_test', 'access_type_test/access_type_test.py', 'access_type_test/access_type_test.c', {'sim': ['RTL', 'GL', 'GL_SDF']}))
-    tests.append(('multi_bank_test', 'multi_bank_test/multi_bank_test.py', 'multi_bank_test/multi_bank_test.c', {'sim': ['RTL', 'GL', 'GL_SDF']}))
-    
-    return tests
+from comprehensive_address_test.comprehensive_address_test import comprehensive_address_test
+from access_type_test.access_type_test import access_type_test
+from multi_bank_test.multi_bank_test import multi_bank_test
